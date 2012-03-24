ca1c7e055c03be6435701caef02d180915bde860
Devin Meadows <dmeadows013@gmail.com>
2012-03-23 19:12:44 -0700

    Deleted call line
    Removes line that calls for /system/lib/acdbmapper.so because our
    device does not include that. This fixes a build error.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -38,7 +38,6 @@ MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \\
-  $OUTDIR/proprietary/lib/libacdbmapper.so:obj/lib/libacdbmapper.so \\
   $OUTDIR/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \\
   $OUTDIR/proprietary/lib/libril.so:obj/lib/libril.so \\
 
