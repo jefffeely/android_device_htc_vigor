d760685dd9b7784929ddef153f5e28895e5a89f5
Jeff Feely <jeffrey.feely@gmail.com>
2012-04-27 12:41:43 -0300

    De-kanged

--- aokp.mk
+++ aokp.mk
@@ -1,15 +1,10 @@
-$(call inherit-product, device/htc/vigor/vigor.mk)
+$(call inherit-product, device/htc/android_device_htc_vigor/vigor.mk)
 
-$(call inherit-product, vendor/aokp/configs/common.mk)
-
-$(call inherit-product, vendor/aokp/configs/common_versions.mk)
-
-PRODUCT_NAME := aokp_vigor
+PRODUCT_NAME := vigor
 PRODUCT_DEVICE := vigor
 PRODUCT_MODEL := PG86100
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vigor BUILD_ID=IML74K BUILD_FINGERPRINT="sprint/htc_shooter/shooter:4.0.3/IML74K/130920.2:user/release-keys" PRIVATE_BUILD_DESC="2.08.651.2 CL130920 release-keys"
-TARGET_BOOTANIMATION_NAME := vertical-720x1280
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vigor
 
 # Release name and versioning
-PRODUCT_RELEASE_NAME := Vigor
+PRODUCT_RELEASE_NAME := vigor
